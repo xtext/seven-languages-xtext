30eb929b0afb07381497589cf85eb463de2d0a4d
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-06-18 18:56:45 +0200

    [template] workaround for bug 382879 https://bugs.eclipse.org/bugs/show_bug.cgi?id=382879

--- template/org.xtext.template/src/org/xtext/template/validation/TemplateJavaValidator.java
+++ template/org.xtext.template/src/org/xtext/template/validation/TemplateJavaValidator.java
@@ -1,13 +1,21 @@
 package org.xtext.template.validation;
- 
 
+import org.eclipse.xtext.xbase.XExpression;
+
+@SuppressWarnings("restriction")
 public class TemplateJavaValidator extends AbstractTemplateJavaValidator {
 
-//	@Check
-//	public void checkGreetingStartsWithCapital(Greeting greeting) {
-//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
-//			warning("Name should start with a capital", MyDslPackage.Literals.GREETING__NAME);
-//		}
-//	}
+	// @Check
+	// public void checkGreetingStartsWithCapital(Greeting greeting) {
+	// if (!Character.isUpperCase(greeting.getName().charAt(0))) {
+	// warning("Name should start with a capital",
+	// MyDslPackage.Literals.GREETING__NAME);
+	// }
+	// }
 
+	@Override
+	protected boolean isImplicitReturn(XExpression expr) {
+		// workaround for https://bugs.eclipse.org/bugs/show_bug.cgi?id=382879
+		return false;
+	}
 }
