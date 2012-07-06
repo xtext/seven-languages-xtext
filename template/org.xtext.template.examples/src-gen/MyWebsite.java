ca9d22a591a97508e891fbcb359eb254856ff488
Jan Koehnlein <jan.koehnlein@itemis.de>
2012-07-06 10:45:21 +0200

    [build] My first bucky fix

--- template/org.xtext.template.examples/src-gen/MyWebsite.java
+++ template/org.xtext.template.examples/src-gen/MyWebsite.java
@@ -52,42 +52,42 @@ public class MyWebsite {
   private CharSequence generate() {
     StringBuilder _appendable = new StringBuilder();
     
-    _appendable.append("-->\n<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"utf-8\">\n\t<title>");
-    _appendable.append(this.title);
-    _appendable.append("</title>\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<meta name=\"description\"\n\t\tcontent=\"");
-    _appendable.append(this.description);
-    _appendable.append("\">\n\t<meta name=\"author\" content=\"");
-    _appendable.append(this.author);
-    _appendable.append("\">\n</head>\n<body>\n");
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("-->\n<!DOCTYPE html>\n<html lang=\"en\">\n<head>\n\t<meta charset=\"utf-8\">\n\t<title>",""));
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(this.title,""));
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("</title>\n\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n\t<meta name=\"description\"\n\t\tcontent=\"",""));
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(this.description,""));
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("\">\n\t<meta name=\"author\" content=\"",""));
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(this.author,""));
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("\">\n</head>\n<body>\n",""));
     StringBuilder _forLoopResult = new StringBuilder();
     for (final Section element : this.sections) {
       StringBuilder _appendable_1 = new StringBuilder();
       
-      _appendable_1.append("\n\t<div class=\"section\">\n\t\t<h1>");
+      _appendable_1.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("\n\t<div class=\"section\">\n\t\t<h1>",""));
       String _title = element.getTitle();
-      _appendable_1.append(_title);
-      _appendable_1.append("</h1>\n\t\t");
+      _appendable_1.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(_title,""));
+      _appendable_1.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("</h1>\n\t\t",""));
       StringBuilder _richstringif = null;
       String _description = element.getDescription();
       boolean _notEquals = (!Objects.equal(_description, null));
       if (_notEquals) {
         StringBuilder _appendable_2 = new StringBuilder();
         
-        _appendable_2.append("\n\t\t\t<p>");
+        _appendable_2.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("\n\t\t\t<p>",""));
         String _description_1 = element.getDescription();
-        _appendable_2.append(_description_1);
-        _appendable_2.append("</p>\n\t\t");
+        _appendable_2.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(_description_1,""));
+        _appendable_2.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("</p>\n\t\t",""));
         _richstringif = _appendable_2;
       }
-      _appendable_1.append(_richstringif);
-      _appendable_1.append("\n\t\t<p>");
+      _appendable_1.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(_richstringif,""));
+      _appendable_1.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("\n\t\t<p>",""));
       String _body = element.getBody();
-      _appendable_1.append(_body);
-      _appendable_1.append("</p>\n\t</div>\n");
+      _appendable_1.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(_body,""));
+      _appendable_1.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("</p>\n\t</div>\n",""));
       _forLoopResult.append(_appendable_1);
     }
-    _appendable.append(_forLoopResult);
-    _appendable.append("\n</body>\n</html>");
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis(_forLoopResult,""));
+    _appendable.append(org.eclipse.xtext.xbase.lib.ObjectExtensions.operator_elvis("\n</body>\n</html>",""));
     return _appendable;
   }
   
