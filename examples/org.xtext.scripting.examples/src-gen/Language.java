a637916ec5e3688cfee99a7a9f76318bcacdeaf1
Moritz Eysholdt <moritz.eysholdt@itemis.de>
2012-10-23 23:22:47 +0200

    simplified the scripting language example

--- examples/org.xtext.scripting.examples/src-gen/Language.java
+++ examples/org.xtext.scripting.examples/src-gen/Language.java
@@ -1,8 +1,6 @@
 import com.google.common.base.Objects;
-import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.List;
 import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 import org.eclipse.xtext.xbase.lib.Functions.Function1;
 import org.eclipse.xtext.xbase.lib.InputOutput;
@@ -28,8 +26,6 @@ public class Language {
     Pair<Integer,String> _mappedTo_1 = Pair.<Integer, String>of(Integer.valueOf(2), "two");
     Pair<Integer,String> _mappedTo_2 = Pair.<Integer, String>of(Integer.valueOf(3), "three");
     /*CollectionLiterals.<Integer, String>newHashMap(_mappedTo, _mappedTo_1, _mappedTo_2);*/
-    /* new BigInteger("beefbeefbeefbeefbeef", 16) */
-    /* new BigDecimal("0.123456789123456789123456789123456789e4242") */
     final String x = "A final value";
     String y = "A non-final variable";
     final String s = "my String";
@@ -37,8 +33,6 @@ public class Language {
     /*(1 - 2);*/
     /*(1 / 2);*/
     /*(1 * 2);*/
-    /*(1 % 2);*/
-    /*Math.pow(1, 2);*/
     boolean _and = false;
     if (!true) {
       _and = false;
@@ -57,8 +51,6 @@ public class Language {
     /*(1 < 2);*/
     /*(1 >= 2);*/
     /*(1 <= 2);*/
-    /*(1 << 2);*/
-    /*(1 >> 2);*/
     /*Pair.<String, String>of("first", "second");*/
     /*new IntegerRange(1, 30);*/
     /*ObjectExtensions.<String>operator_elvis(
@@ -97,66 +89,42 @@ public class Language {
         }
       };
     /*ListExtensions.<String, String>map(list, _function_3);*/
+    String _xifexpression = null;
     int _length = "foo".length();
     boolean _greaterThan = (_length > 3);
     if (_greaterThan) {
-      /* "never true!" */
+      _xifexpression = "never true!";
     } else {
-      /* "always true!" */
+      _xifexpression = "always true!";
     }
-    final ArrayList<String> list_1 = CollectionLiterals.<String>newArrayList("foo", "bar", "baz");
-    ArrayList<String> _arrayList = new ArrayList<String>();
-    final ArrayList<String> result = _arrayList;
-    List<String> _reverse = ListExtensions.<String>reverse(list);
-    for (final String x_2 : _reverse) {
-      String _upperCase = x_2.toUpperCase();
-      result.add(_upperCase);
-    }
-    /*result;*/
-    int i = 0;
-    int _size = list.size();
-    boolean _lessThan = (i < _size);
-    boolean _while = _lessThan;
-    while (_while) {
-      {
-        String _get = list.get(i);
-        String _plus = ("whiled-" + _get);
-        result.add(_plus);
-        int _plus_1 = (i + 1);
-        i = _plus_1;
-      }
-      int _size_1 = list.size();
-      boolean _lessThan_1 = (i < _size_1);
-      _while = _lessThan_1;
-    }
-    /*result;*/
+    final String myString = _xifexpression;
     final ArrayList<Object> itemis = CollectionLiterals.<Object>newArrayList("foo", "bar", Integer.valueOf(123), BigInteger.valueOf(123456789L));
-    for (final Object i_1 : itemis) {
+    for (final Object i : itemis) {
       boolean _matched = false;
       if (!_matched) {
-        if (Objects.equal(i_1,"foo")) {
+        if (Objects.equal(i,"foo")) {
           _matched=true;
           /* "it\'s foo" */
         }
       }
       if (!_matched) {
-        if (Objects.equal(i_1,"bar")) {
+        if (Objects.equal(i,"bar")) {
           _matched=true;
           /* "a bar" */
         }
       }
       if (!_matched) {
-        if (i_1 instanceof Integer) {
-          final Integer _integer = (Integer)i_1;
-          if (Objects.equal(i_1,123)) {
+        if (i instanceof Integer) {
+          final Integer _integer = (Integer)i;
+          if (Objects.equal(i,123)) {
             _matched=true;
             /* "found integer" */
           }
         }
       }
       if (!_matched) {
-        if (i_1 instanceof BigInteger) {
-          final BigInteger _bigInteger = (BigInteger)i_1;
+        if (i instanceof BigInteger) {
+          final BigInteger _bigInteger = (BigInteger)i;
           _matched=true;
           int _bitLength = _bigInteger.bitLength();
           /*("it\'s huge, soo long:" + Integer.valueOf(_bitLength));*/
