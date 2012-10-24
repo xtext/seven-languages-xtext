b6781260799efc6e60954b0ddbecd90b2ec8cc01
Holger Schill <Holger.Schill@itemis.de>
2012-10-24 09:43:50 +0200

    some more examples

---
+++ examples/org.xtext.httprouting.examples/src-gen/routes/Simple.java
@@ -0,0 +1,48 @@
+package routes;
+
+import com.google.inject.Inject;
+import java.util.regex.Pattern;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import simple.SimpleClazz;
+
+@SuppressWarnings("serial")
+public class Simple extends HttpServlet {
+  @Inject
+  private SimpleClazz simpleClazz;
+
+  private static Pattern _pattern1 = Pattern.compile("/(\\w+)");
+
+  public void _doGet1(final HttpServletRequest request, final HttpServletResponse response, final String someValue) {
+    SimpleClazz.staticMethod(someValue);
+  }
+
+  private static Pattern _pattern3 = Pattern.compile("/(\\w+)");
+
+  public void _doGet3(final HttpServletRequest request, final HttpServletResponse response, final String someValue) {
+    this.simpleClazz.instanceMethod(someValue);
+  }
+
+  @Override
+  public void doGet(final HttpServletRequest request, final HttpServletResponse response) {
+    String url =  request.getRequestURL().toString();
+    {
+	java.util.regex.Matcher _matcher = _pattern1.matcher(url);
+	if (_matcher.find()) {
+		String someValue = _matcher.group(1);
+		_doGet1(request, response, someValue);
+		return;
+	}
+    }
+    {
+	java.util.regex.Matcher _matcher = _pattern3.matcher(url);
+	if (_matcher.find()) {
+		String someValue = _matcher.group(1);
+		_doGet3(request, response, someValue);
+		return;
+	}
+    }
+
+  }
+}
---
+++ examples/org.xtext.httprouting.examples/src/simple/SimpleClazz.xtend
@@ -0,0 +1,6 @@
+package simple
+
+class SimpleClazz {
+	def  static void staticMethod(String s){}
+	def void instanceMethod(String s){}
+}
